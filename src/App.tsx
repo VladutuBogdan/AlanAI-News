914cdbe4aa20718e1ab93578cfb798c8b89b8ccd
VladutuBogdan <bogdanvladutu@gmail.com>
2023-02-07 18:57:38 +0200

    Dark text changed

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App = () =>  {
 
   return (
     <div>
-      <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center mt-2 dark:text-white">Alan AI News</h1>
+      <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center mt-2">Alan AI News</h1>
       <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
   );
